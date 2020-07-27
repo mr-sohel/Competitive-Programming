2ad45040ec494814ec61e81dc962c8f7535af042
Taalpatar-Shepai <sohelr2054@gmail.com>
2020-07-27 22:15:01 +0600

    new commit

--- Codeforces/A. Night at the Muesum.cpp
+++ Codeforces/A. Night at the Muesum.cpp
@@ -5,21 +5,22 @@
 
 using namespace std;
 
+//i could't find the solutin, i had to see the editorial to solve.
+
 int main()
 {
     faster_io;
     string name;
     cin>>name;
-    int start = 0, ans = 0;
+    int flag = 97, ans = 0;
     for (int i = 0; i < name.length(); i++)
     {
-        int index = name[i] - 97;
-        int walk = abs(start - index);
+        //int index = name[i] - 97;
+        int walk = abs(flag - name[i]);
         if(walk > 13)
-            ans += 26 - walk;
-        else
-            ans += index;
-        start = index;
+            walk = 26 - walk;
+        ans += walk;
+        flag = name[i];
     }
     cout<<ans<<endl;
     return 0;
