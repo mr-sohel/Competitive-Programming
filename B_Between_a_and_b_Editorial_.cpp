3f160265a5f74714ed1c6dadbfb7752279de17b9
Taalpatar-Shepai <sohelr2054@gmail.com>
2020-08-12 08:54:21 +0600

    new commit

---
+++ B_Between_a_and_b_Editorial_.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+#define endl        '\n'
+#define pi          2*acos(0.0)
+#define pb(a)       push_back(a)
+#define db          double
+#define ld          long double
+#define ll          long long
+#define ull         unsigned long long
+#define mod         1000000007
+#define inf         1000000000000000001
+#define sqr(x)      (x) * (x)
+#define gcd(a, b)   __gcd(a, b)
+#define lcm(a, b)   ((a/gcd(a,b)) * b)
+#define debug(x) 	cerr << #x << " = " << (x) <<endl
+#define all(x)      (x).begin(), (x).end()
+#define unsyncIO    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+
+
+using namespace std;
+
+int main()
+{
+    //freopen("/home/taalpatar_shepai/Documents/my_codes/out.txt", "w", stdout);
+    ull a, b, x, cnt = 0;
+    cin >> a >> b >> x;
+    
+    
+    cout << cnt << endl;
+
+    return 0;
+}
