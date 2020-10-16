964c5a0559ac0be898bbffdf5e6d667ae829b900
Taalpatar-Shepai <sohelr2054@gmail.com>
2020-10-16 19:03:15 +0600

    Accepted

---
+++ 494_Kindergarten_Counting_Game.cpp
@@ -0,0 +1,53 @@
+/**
+ *    Author:  Sohel Rana
+ *    Date:    2020-10-15 21:25:06
+ *	  Link:    link
+**/
+#include <bits/stdc++.h>
+#define endl        '\n'
+#define db          double
+#define ld          long double
+#define ll          long long
+#define ull         unsigned long long
+#define sqr(x)      (x) * (x)
+#define gcd(a, b)   __gcd(a, b)
+#define lcm(a, b)   ((a/gcd(a,b)) * b)
+#define pf(x) 		push_front(x)
+#define pb(x)       push_back(x)
+#define eb(x)		emplace_back(x)
+#define all(x)      (x).begin(), (x).end()
+#define rall(x) 	(x).rbegin(), (x).rend()
+#define sz(x) 		(int)x.size()
+#define debug(x) 	cerr << #x << " = " << (x) <<endl
+#define unsyncIO    ios_base::sync_with_stdio(0); cin.tie(0)
+
+const ld PI = acos((ld)-1);
+const int MOD = 1e9+7;
+const ll INF = 1e18; 
+
+using namespace std;
+
+int main()
+{
+    //freopen("/home/taalpatar_shepai/Documents/my_codes/out.txt", "w", stdout);
+    unsyncIO;
+    
+    string s, temp;
+
+    while(getline(cin, s)) {
+        int cnt = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if(ispunct(s[i]) || isdigit(s[i]))
+                s[i] = ' ';
+        }
+        for (int i = 0; i < s.length()-1; i++) {
+            if(s[i] == ' ' && isalpha(s[i+1]))
+                cnt++;
+        }
+        if(s[0] == ' ')
+            cout << cnt << endl;
+        else
+            cout << cnt + 1 << endl;
+    }
+        return 0;
+}
