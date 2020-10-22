ef42405fc1361eda9a3404c4d04069779ab62d17
Taalpatar-Shepai <sohelr2054@gmail.com>
2020-10-22 20:57:25 +0600

    no commit

---
+++ Dimik OJ/Khoj The Search 1.cpp
@@ -0,0 +1,44 @@
+/**
+ *    Author:  Sohel Rana
+ *    Date:    2020-10-22 17:56:07
+ *	  Link:    link
+**/
+#include <bits/stdc++.h>
+#define endl        '\n'
+#define db          double
+#define ld          long double
+#define ll          long long
+#define ull         unsigned long long
+#define sqr(x)      (x) * (x)
+#define gcd(a, b)   __gcd(a, b)
+#define lcm(a, b)   ((a/gcd(a,b)) * b)
+#define pf(x) 		push_front(x)
+#define pb(x)       push_back(x)
+#define eb(x)		emplace_back(x)
+#define all(x)      (x).begin(), (x).end()
+#define rall(x) 	(x).rbegin(), (x).rend()
+#define sz(x) 		(int)x.size()
+#define debug(x) 	cerr << #x << " = " << (x) << endl
+#define debug2(x,y) cerr << #x << " = " << (x)<< "," << #y << " = " <<(y)<< endl
+#define unsyncIO    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+
+const ld PI = acos((ld)-1);
+const int MOD = 1e9+7  ;
+const ll INF = 1e18; 
+
+using namespace std;
+
+int main()
+{
+    //freopen("in.txt", "r", stdin);
+    //freopen("out.txt", "w", stdout);
+    //unsyncIO;
+    int t;
+    cin >> t;
+    while (t--) {
+        string s1, s2;
+        cin >> s1 >> s2;
+        cout << s1.find(s2) << endl;
+    }
+    return 0;
+}
