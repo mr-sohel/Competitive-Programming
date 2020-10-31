74631fc015af37a27b3861ff627c5b348e0a464b
Taalpatar-Shepai <sohelr2054@gmail.com>
2020-10-31 13:00:12 +0600

    Accepted

--- UVa Online Judge/G_Andy_s_First_Dictionary.cpp
+++ UVa Online Judge/G_Andy_s_First_Dictionary.cpp
@@ -1,32 +1,29 @@
 /**
  *	Author:  Sohel Rana
- *	Date:    2020-10-30 23:26:43
+ *	Date:    2020-10-31 12:56:33
  *	Task:    G_Andy_s_First_Dictionary 
 **/
 #include <bits/stdc++.h>
-#define endl '\n'
-#define db double
-#define ld long double
-#define ll long long
-#define ull unsigned long long
-#define sqr(x) (x) * (x)
-#define gcd(a, b) __gcd(a, b)
-#define lcm(a, b) ((a / gcd(a, b)) * b)
-#define pf(x) push_front(x)
-#define pb(x) push_back(x)
-#define eb(x) emplace_back(x)
-#define all(x) (x).begin(), (x).end()
-#define rall(x) (x).rbegin(), (x).rend()
-#define sz(x) (int)x.size()
-#define debug(x) cerr << #x << " = " << (x) << endl
-#define debug2(x, y) cerr << #x << " = " << (x) << "," << #y << " = " << (y) << endl
-#define unsyncIO                  \
-    ios_base::sync_with_stdio(0); \
-    cin.tie(0);                   \
-    cout.tie(0)
+#define endl        '\n'
+#define db          double
+#define ld          long double
+#define ll          long long
+#define ull         unsigned long long
+#define sqr(x)      (x) * (x)
+#define gcd(a, b)   __gcd(a, b)
+#define lcm(a, b)   ((a/gcd(a,b)) * b)
+#define pf(x) 		push_front(x)
+#define pb(x)       push_back(x)
+#define eb(x)		emplace_back(x)
+#define all(x)      (x).begin(), (x).end()
+#define rall(x) 	(x).rbegin(), (x).rend()
+#define sz(x) 		(int)x.size()
+#define debug(x) 	cerr << #x << " = " << (x) << endl
+#define debug2(x,y) cerr << #x << " = " << (x)<< "," << #y << " = " <<(y)<< endl
+#define unsyncIO    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
 
 const ld PI = acos((ld)-1);
-const int MOD = 1e9 + 7;
+const int MOD = 1e9+7;
 const ll INF = 1e18;
 const ld eps = 1.0E-14;
 
