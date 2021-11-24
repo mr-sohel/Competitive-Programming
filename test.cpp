11015deb4f522c6388acb0c209b2465776e2b71d
Taalpatar-Shepai <sohelr2054@gmail.com>
2021-11-24 22:16:40 +0600

    new commit

--- test.cpp
+++ test.cpp
@@ -1,6 +1,6 @@
 /**
  *	Author:  Sohel Rana
- *	Date:    2021-11-22 17:22:24
+ *	Date:    2021-11-24 22:13:31
  *	Task:    test 
 **/
 #include <bits/stdc++.h>
@@ -8,37 +8,33 @@
 #define sqr(x)      (x) * (x)
 #define gcd(a, b)   __gcd(a, b)
 #define lcm(a, b)   ((a/gcd(a,b)) * b)
-#define pf(x) 		push_front(x)
+#define pf(x) 	  	push_front(x)
 #define pb(x)       push_back(x)
-#define eb(x)		emplace_back(x)
+#define eb(x)		    emplace_back(x)
 #define all(x)      (x).begin(), (x).end()
-#define rall(x) 	(x).rbegin(), (x).rend()
-#define sz(x) 		(int)x.size()
-#define debug(x) 	cerr << #x << " = " << (x) << endl
+#define rall(x) 	  (x).rbegin(), (x).rend()
+#define sz(x) 		  (int)x.size()
+#define debug(x)  	cerr << #x << " = " << (x) << endl
 #define debug2(x,y) cerr << #x << " = " << (x)<< "," << #y << " = " <<(y)<< endl
 #define unsyncIO    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
 
-typedef double 			   db;
-typedef long double 	   	   ld;
-typedef long long 		   ll;
+typedef double 			           db;
+typedef long double 	   	     ld;
+typedef long long 		         ll;
 typedef unsigned long long 	   ull;
+
 const ld PI = acos((ld)-1);
 const int MOD = 1e9+7;
 const long long INF = 1e18;
-const ld eps = 1.0E-14;
+const ld eps = 1.0e-14;
 
 using namespace std;
 
-int main()
+int main() 
 {
-    //freopen("in.txt", "r", stdin);
-    //freopen("out.txt", "w", stdout);
-    //unsyncIO;
-    int cnt = 0;
-    for(int i = 1; i <= 100; i++) {
-        if(i % 5 != 0 and i % 7 != 0 and i % 3 != 0)
-            cnt++; 
-    }
-    cout<<cnt<<endl;
+  //freopen("in.txt", "r", stdin);
+  //freopen("out.txt", "w", stdout);
+  //unsyncIO;
+  
     return 0;
 }
