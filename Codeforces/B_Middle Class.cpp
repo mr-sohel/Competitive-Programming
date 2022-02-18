ba5dacb27a581f23a5ba3584ac99dc5f527318c3
Taalpatar-Shepai <sohelr2054@gmail.com>
2022-02-18 22:59:20 +0600

    Accepted

---
+++ Codeforces/B_Middle Class.cpp
@@ -0,0 +1,63 @@
+/**
+ *	Author:  Sohel Rana
+ *	Date:    2022-02-18 10:27:44
+ *	Task:    B_Middle Class 
+**/
+#include <bits/stdc++.h>
+#define endl			'\n'
+#define sqr(x)			(x) * (x)
+#define gcd(x, y)		__gcd(x, y)
+#define lcm(x, y)		((x/gcd(x,y)) * y)
+#define pf(x) 	  		push_front(x)
+#define pb(x)			push_back(x)
+#define eb(x)			emplace_back(x)
+#define all(x)      	(x).begin(), (x).end()
+#define rall(x)			(x).rbegin(), (x).rend()
+#define sz(x)			(int)x.size()
+#define debug(x)		cerr<<#x<<" = "<<(x)<< endl
+#define debug2(x,y) 	cerr<<#x<<" = "<<(x)<<","<<#y<<" = "<<(y)<< endl
+#define unsyncIO    	ios_base::sync_with_stdio(false); cin.tie(nullptr)
+
+using ll = long long;
+using db = double;
+using ld = long double;
+using ull = unsigned long long;
+
+const ld PI = acos((ld)-1);
+const int MOD = 1e9+7;
+const long long INF = 1e18;
+const ld EPS = 1e-9;
+
+using namespace std;
+
+
+int main() 
+{
+    //freopen("in.txt", "r", stdin);
+    //freopen("out.txt", "w", stdout);
+    //unsyncIO;
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, x;
+        ll sum = 0LL;
+        cin >> n >> x;
+        vector<int> v(n);
+        for (int i = 0; i < n;i++) {
+            cin >> v[i];
+            sum += v[i];
+        }
+        sort(all(v));
+        int ans = 0;
+        for (int i = 0; i <= n;i++) {
+            db div = (db)sum / (n - i);
+            if (div >= x) {
+                ans = n - i;
+                break;
+            }
+            sum -= v[i];
+        }
+        cout << ans << endl;
+    }
+    return 0;
+}
