5c7b0960aff8fd5a1bf299c8f0d7b5412093f565
Taalpatar-Shepai <sohelr2054@gmail.com>
2022-02-20 20:05:55 +0600

    Updated

--- tamplate.cpp
+++ tamplate.cpp
@@ -1,32 +1,34 @@
 /**
- *	Author:  Sohel Rana
- *	Date:    2021-12-23 14:54:04
- *	Task:    tamplate 
+ *	Author:  ${1:Sohel Rana}
+ *	Date:    ${date}
+ *	Task:    ${TM_FILENAME_BASE} 
 **/
 #include <bits/stdc++.h>
-#define endl			'\n'
-#define sqr(x)			(x) * (x)
-#define gcd(x, y)		__gcd(x, y)
-#define lcm(x, y)		((x/gcd(x,y)) * y)
-#define pf(x) 	  		push_front(x)
-#define pb(x)			push_back(x)
-#define eb(x)			emplace_back(x)
-#define all(x)      	(x).begin(), (x).end()
-#define rall(x)			(x).rbegin(), (x).rend()
-#define sz(x)			(int)x.size()
-#define debug(x)		cerr<<#x<<" = "<<(x)<< endl
-#define debug2(x,y) 	cerr<<#x<<" = "<<(x)<<","<<#y<<" = "<<(y)<< endl
-#define unsyncIO    	ios_base::sync_with_stdio(false); cin.tie(nullptr)
+#define endl		'\n'
+#define sqr(x)		(x) * (x)
+#define gcd(x,y)	__gcd(x, y)
+#define lcm(x,y)	((x/gcd(x,y)) * y)
+#define pf(x)		push_front(x)
+#define pb(x)		push_back(x)
+#define eb(x)		emplace_back(x)
+#define all(x)		(x).begin(), (x).end()
+#define rall(x)		(x).rbegin(), (x).rend()
+#define sz(x)		(int)x.size()
+#define prec(x) 	fixed<<setprecision(x)
+#define debug(x)	cerr<<#x<<" = "<<(x)<< endl
+#define debug(x,y)	cerr<<#x<<" = "<<(x)<<","<<#y<<" = "<<(y)<< endl
+#define unsyncIO	ios_base::sync_with_stdio(false); cin.tie(nullptr)
 
 using ll = long long;
 using db = double;
 using ld = long double;
 using ull = unsigned long long;
 
-const ld PI = acos((ld)-1);
-const int MOD = 1e9+7;
-const long long INF = 1e18;
-const ld EPS = 1e-9;
+const ld pi = acos((ld)-1);
+const int mod = 1e9+7;
+const ll inf = 1e18;
+const ld eps = 1e-9;
+const int mx = 1e5;
 
 using namespace std;
 
@@ -37,5 +39,5 @@ int main()
 	//freopen("out.txt", "w", stdout);
 	//unsyncIO;
 	
-	return 0;
+    return 0;
 }
--- test.cpp
+++ test.cpp
@@ -1,10 +1,10 @@
 /**
  *	Author:  Sohel Rana
- *	Date:    2022-02-19 23:01:42
+ *	Date:    2022-02-20 20:04:36
  *	Task:    test 
 **/
 #include <bits/stdc++.h>
-#define endl            '\n'
+#define endl		'\n'
 #define sqr(x)		(x) * (x)
 #define gcd(x,y)	__gcd(x, y)
 #define lcm(x,y)	((x/gcd(x,y)) * y)
@@ -14,8 +14,9 @@
 #define all(x)		(x).begin(), (x).end()
 #define rall(x)		(x).rbegin(), (x).rend()
 #define sz(x)		(int)x.size()
+#define prec(x) 	fixed<<setprecision(x)
 #define debug(x)	cerr<<#x<<" = "<<(x)<< endl
-#define debug2(x,y)     cerr<<#x<<" = "<<(x)<<","<<#y<<" = "<<(y)<< endl
+#define debug(x,y)	cerr<<#x<<" = "<<(x)<<","<<#y<<" = "<<(y)<< endl
 #define unsyncIO	ios_base::sync_with_stdio(false); cin.tie(nullptr)
 
 using ll = long long;
@@ -23,19 +24,20 @@ using db = double;
 using ld = long double;
 using ull = unsigned long long;
 
-const ld PI = acos((ld)-1);
-const int MOD = 1e9+7;
-const long long INF = 1e18;
-const ld EPS = 1e-9;
+const ld pi = acos((ld)-1);
+const int mod = 1e9+7;
+const ll inf = 1e18;
+const ld eps = 1e-9;
+const int mx = 1e5;
 
 using namespace std;
 
 
 int main() 
 {
-    //freopen("in.txt", "r", stdin);
-    //freopen("out.txt", "w", stdout);
-    //unsyncIO;
-    
-    return 0;
+	//freopen("in.txt", "r", stdin);
+	//freopen("out.txt", "w", stdout);
+	//unsyncIO;
+	
+	return 0;
 }
