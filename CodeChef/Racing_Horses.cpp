82286f889736251923ad7a36259352bf63bb6506
Taalpatar-Shepai <sohelr2054@gmail.com>
2022-03-29 22:45:29 +0600

    Practice

---
+++ CodeChef/Racing_Horses.cpp
@@ -0,0 +1,57 @@
+/**
+ *	Author:  Sohel Rana
+ *	Date:    2022-03-29 22:40:34
+ *	Task:    Racing_Horses 
+**/
+#include <bits/stdc++.h>
+#define endl		'\n'
+#define sqr(x)		(x) * (x)
+#define gcd(x,y)	__gcd(x, y)
+#define lcm(x,y)	((x/gcd(x,y)) * y)
+#define pf(x)		push_front(x)
+#define pb(x)		push_back(x)
+#define eb(x)		emplace_back(x)
+#define all(x)		(x).begin(), (x).end()
+#define rall(x)		(x).rbegin(), (x).rend()
+#define sz(x)		(int)x.size()
+#define prec(x) 	fixed<<setprecision(x)
+#define debug(x)	cerr<<#x<<" = "<<(x)<< endl
+#define debug2(x,y)	cerr<<#x<<" = "<<(x)<<","<<#y<<" = "<<(y)<< endl
+#define unsyncIO	ios_base::sync_with_stdio(false); cin.tie(nullptr)
+
+using ll = long long;
+using db = double;
+using ld = long double;
+using ull = unsigned long long;
+
+const ld pi = acos((ld)-1);
+const int mod = 1e9+7;
+const ll inf = 1e18;
+const ld eps = 1e-9;
+const int mx = 1e5;
+
+using namespace std;
+
+
+int main() 
+{
+    //freopen("in.txt", "r", stdin);
+    //freopen("out.txt", "w", stdout);
+    //unsyncIO;
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<int> vec(n);
+        for (int i = 0;i < n;i++) cin >> vec[i];
+        sort(all(vec));
+        int res = INT_MAX;
+        for (int i = 0; i < n - 1;i++) {
+            if ((vec[i + 1] - vec[i]) < res)
+                res = (vec[i + 1] - vec[i]);
+        }
+        cout << res << endl;
+    }
+    return 0;
+}
