2ed523e43127db1840c70fcb57f2c250a18173e7
mr-sohel <mrsohelcse@gmail.com>
2023-07-04 14:05:04 +0600

    Next Greater Element

---
+++ Template Library/STL/Stack/Next Greater Element.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const int N = 1e6 + 5;
+
+int a[N], ans[N];
+
+void solve() {
+	int n;
+	cin >> n;
+	for (int i = 1; i <= n; i++) {
+		cin >> a[i];
+	}
+	stack<int> st;
+	for (int i = n; i >= 0; i--) {
+		while (!st.empty() and a[st.top()] <= a[i]) {
+			st.pop();
+		}
+		if (st.empty()) {
+			ans[i] = -1;
+		} else {
+			ans[i] = st.top();
+		}
+		st.push(i); // pushing index
+	}
+	for (int i = 1; i <= n; i++) {
+		cout << ans[i] << " ";
+	}
+}
+
+int main() {
+	int t = 1;
+	// cin >> t;
+	while (t--) {
+		solve();
+	}
+	return 0;
+}
