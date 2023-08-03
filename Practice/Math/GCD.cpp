04cd6071a4c8afde3e69ac7ffc06a67d58150892
mr-sohel <mrsohelcse@gmail.com>
2023-08-03 22:34:42 +0600

    update

--- Practice/Math/GCD.cpp
+++ Practice/Math/GCD.cpp
@@ -2,26 +2,19 @@
 
 using namespace std;
 
+int gcd(int a, int b) { //assuming a >= b
+	return b == 0 ? a : gcd(b, a % b);
+}
+
 int main()
 {
-    int n1,n2, reminder;
-    int t;
-    cin>>t;
-    cin>>n1>>n2;
-    if(n1 < n2)
-        swap(n1,n2);
-        
-    while(true)
-    {
-        reminder = n2 % n1;
-        if(reminder == 0)
-            break;
-        else
-        {
-            n2 = n1;
-            n1 = reminder;
-        }
-    }
-    cout<<n1<<endl;
+    int a,b, ans;
+    cin>>a>>b;
+    if(a < b)
+      swap(a,b);
+    ans = __gcd(a,b);  
+ //    ans = gcd(a,b);
+   
+    cout<<ans<<endl;
     return 0;
 }
