af28ee0c59e49e4d3f2222310f4adb411124375d
mr-sohel <mrsohelcse@gmail.com>
2024-06-16 14:02:52 +0600

    new

--- code.cpp
+++ code.cpp
@@ -1,20 +1,65 @@
-#include<bits/stdc++.h>
+//CR7_G.O.A.T
+#include <bits/stdc++.h>
 using namespace std;
-
+const int N = 1000 + 5;
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
 
-    int t;
-    cin >> t;
-    while (t--) {
-        int n, m;
-        cin >> n >> m;
-        if (m >= n) {
-            cout << (1 << (n + 1)) - 1 << "\n";
-        } else {
-            cout << (1 << (m + 1)) - 1 << "\n";
+    vector<bool> is_prime(N + 1, true);
+
+    is_prime[0] = is_prime[1] = false;
+    for (int i = 2; i * i <= N; i++) {
+        if (is_prime[i] == true) {
+            for (int j = i * i; j <= N; j = j + i) {
+                is_prime[j] = false;
+            }
+
         }
+
     }
+    vector<long long>primes;
+    for (int i = 0; i <= N; i++) {
+        if (is_prime[i]) {
+            bool f = false;
+            long long num = i, tmp = 0, cnt = 1;
+            while (num > 1)
+            {
+                tmp = tmp + (cnt * (num % 10));
+                cnt *= 10;
+                if (is_prime[tmp] == false || num % 10 == 0)
+                {
+                    f = true;
+                    break;
+                }
+
+                num /= 10;
+
+            }
+
+            if (f == false) primes.push_back(i);
+        }
+
+    }
+    for (auto it : primes) cout << it << '\n';
+
+    // long long q, k; cin >> q;
+    // while (q--) {
+    //     cin >> k;
+    //     long long lo = 0, hi = primes.size() - 1, mid, ans = -1;
+    //     while (hi - lo >= 0) {
+    //         mid = lo + (hi - lo) / 2;
+    //         if (primes[mid] > k) {
+    //             hi = mid - 1;
+    //         } else {
+    //             lo = mid + 1;
+    //             ans = mid;
+    //         }
+    //     }
+    //     // cout << lo << '\n'; // is also correct
+    //     cout << ans + 1 << '\n';
+    // }
+
     return 0;
 }
--- sol.cpp
+++ sol.cpp
@@ -19,7 +19,6 @@ using minHeap = priority_queue<T, vector<T>, greater<T>>;
 const ld PI = acos(-1.0);
 const ll MOD = 1e9 + 7;
 const ld EPS = 1e-9;
-const ll N = 2e5 + 5;
 int tc = 1;
 
 #ifdef LOCAL
@@ -28,17 +27,67 @@ int tc = 1;
 #define debug(...)
 #endif
 
+bool check(int n);
+const int N = 1e6; // upto 1e8
+bitset < N + 5 > mark;
+vector<ll>primes;
+void sieve() {
+   int sq = sqrt(N);
+   for (int i = 4; i <= N; i += 2) {
+      mark[i] = true;
+   }
+   for (int i = 3; i <= sq; i += 2) {
+      if (!mark[i]) {
+         for (int j = i * i; j <= N; j += (i << 1)) {
+            mark[j] = true;
+         }
+      }
+   }
+   primes.push_back(2);
+
+   for (int i = 3; i <= N; i += 2) {
+      if (!mark[i] and check(i))
+         primes.push_back(i);
+   }
+}
+bool isPrime(int n) {
+   if (n < 2) return false;
+   return !mark[n];
+}
+bool check(int n) {
+   if (n < 10) return true;
+   string temp = to_string(n);
+   for (auto &it : temp) {
+      if (it == '0') return false;
+   }
+   while (sz(temp) >= 1) {
+      if (isPrime(stoi(temp)) == false) return false;
+      temp = temp.substr(1, sz(temp));
+   }
+   return true;
+}
+
+
 void solve() {
-   int a = 11;
-   a = a & (~(1 << 3));
-   cout << a << '\n';
+   int k; cin >> k;
+   int lo = 0, hi = sz(primes) - 1, ans = -1;
+   while (lo <= hi) {
+      int mid = lo + ((hi - lo) / 2);
+      if (primes[mid] > k) {
+         hi = mid - 1;
+         ans = mid;
+      } else {
+         lo = mid + 1;
+      }
+   }
+   cout << lo << '\n';
 }
 
 int main() {
    unsyncIO;
-
+   sieve();
    int t = 1;
-   //cin >> t;
+   cin >> t;
    while (t--) {
       solve();
    }
